69b582eb5edf1bd5ca3d305f356767a84dc5f0b8
Marcelo Duarte Trevisani <marcelotrevisani@users.noreply.github.com>
2018-10-10 11:39:14 +0200

    Add options to build

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,6 @@ set -ex
              --verbose \
              --prefix="${PREFIX}" \
              --system-libs \
-             --system-libuv \
              --no-qt-gui \
              --no-system-libarchive \
              --no-system-jsoncpp \
@@ -15,5 +14,10 @@ set -ex
              -DCMAKE_FIND_ROOT_PATH="${PREFIX}" \
              -DCMAKE_INSTALL_RPATH="${PREFIX}/lib" \
              -DCURSES_INCLUDE_PATH="${PREFIX}/include" \
-             -DBUILD_CursesDialog=ON
+             -DBUILD_CursesDialog=ON \
+             -DCMake_HAVE_CXX_MAKE_UNIQUE:INTERNAL=TRUE \
+             -DCMAKE_PREFIX_PATH="${PREFIX}" \
+             -DCMAKE_CXX_STANDARD:STRING=17
+             
+             
 make install -j${CPU_COUNT}
