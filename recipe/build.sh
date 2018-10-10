b392c6e952b23b8c3bac981ce296a76af037f353
Marcelo Duarte Trevisani <marcelotrevisani@users.noreply.github.com>
2018-10-10 18:44:32 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -15,7 +15,7 @@ set -ex
              -DCMAKE_INSTALL_RPATH="${PREFIX}/lib" \
              -DCURSES_INCLUDE_PATH="${PREFIX}/include" \
              -DBUILD_CursesDialog=ON \
-             -DCMake_HAVE_CXX_MAKE_UNIQUE:INTERNAL=TRUE \
+             -DCMake_HAVE_CXX_MAKE_UNIQUE:INTERNAL=FALSE \
              -DCMAKE_PREFIX_PATH="${PREFIX}" \
              -DCMAKE_CXX_STANDARD:STRING=17
              
