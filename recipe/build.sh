484baa9ca9e693c42b0133e18b4993af6900adf1
Peter Williams <peter@newton.cx>
2018-10-16 11:53:12 -0400

    recipe/build.sh: fix GCC7 builds
    The new compiler chain automatically adds $PREFIX/include to the preprocessor
    search path, which breaks the bootstrap build.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 set -ex
 
+# The CMake bootstrap build requires an internal version of libuv. If $CFLAGS
+# and friends include -I$PREFIX/include, the build picks up the real libuv
+# headers first, which breaks the build. The real build adds the needed
+# include paths correctly.
+export CFLAGS=$(echo "$CFLAGS" |sed -e "s|-I$PREFIX/include||")
+export DEBUG_CFLAGS=$(echo "$DEBUG_CFLAGS" |sed -e "s|-I$PREFIX/include||")
+export CXXFLAGS=$(echo "$CXXFLAGS" |sed -e "s|-I$PREFIX/include||")
+export DEBUG_CXXFLAGS=$(echo "$DEBUG_CXXFLAGS" |sed -e "s|-I$PREFIX/include||")
+
 ./bootstrap \
              --verbose \
              --prefix="${PREFIX}" \
@@ -18,6 +27,5 @@ set -ex
              -DCMake_HAVE_CXX_MAKE_UNIQUE:INTERNAL=FALSE \
              -DCMAKE_PREFIX_PATH="${PREFIX}" \
              -DCMAKE_CXX_STANDARD:STRING=17
-             
-             
+
 make install -j${CPU_COUNT}
