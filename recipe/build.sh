cc040319de64b26375c495b5016d21e01a6ccdc6
Isuru Fernando <isuruf@gmail.com>
2020-10-14 13:44:52 -0500

    Add cross compile support

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,23 +1,32 @@
 #!/bin/sh
 set -ex
 
-./bootstrap \
-             --verbose \
-             --prefix="${PREFIX}" \
-             --system-libs \
-             --no-qt-gui \
-             --no-system-libarchive \
-             --no-system-jsoncpp \
-             --parallel=${CPU_COUNT} \
-             -- \
-             -DCMAKE_BUILD_TYPE:STRING=Release \
-             -DCMAKE_FIND_ROOT_PATH="${PREFIX}" \
-             -DCMAKE_INSTALL_RPATH="${PREFIX}/lib" \
-             -DCURSES_INCLUDE_PATH="${PREFIX}/include" \
-             -DBUILD_CursesDialog=ON \
-             -DCMake_HAVE_CXX_MAKE_UNIQUE:INTERNAL=FALSE \
-             -DCMAKE_PREFIX_PATH="${PREFIX}"
-             
+CMAKE_ARGS="$CMAKE_ARGS -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_FIND_ROOT_PATH=${PREFIX} -DCMAKE_INSTALL_RPATH=${PREFIX}/lib"
+CMAKE_ARGS="$CMAKE_ARGS -DCURSES_INCLUDE_PATH=${PREFIX}/include -DBUILD_CursesDialog=ON -DCMake_HAVE_CXX_MAKE_UNIQUE:INTERNAL=FALSE"
+CMAKE_ARGS="$CMAKE_ARGS -DCMAKE_PREFIX_PATH=${PREFIX}"
+
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
+   cmake ${CMAKE_ARGS} \
+       -DCMAKE_VERBOSE_MAKEFILE=1 \
+       -DCMAKE_INSTALL_PREFIX=$PREFIX \
+       -DCMAKE_USE_SYSTEM_LIBRARIES=ON \
+       -DBUILD_QtDialog=OFF \
+       -DCMAKE_USE_SYSTEM_LIBARCHIVE=OFF \
+       -DCMAKE_USE_SYSTEM_JSONCPP=OFF \
+       .
+else
+  ./bootstrap \
+       --verbose \
+       --prefix="${PREFIX}" \
+       --system-libs \
+       --no-qt-gui \
+       --no-system-libarchive \
+       --no-system-jsoncpp \
+       --parallel=${CPU_COUNT} \
+       -- \
+       ${CMAKE_ARGS}
+fi
+
 # CMake automatically selects the highest C++ standard available
 
 make install -j${CPU_COUNT}
